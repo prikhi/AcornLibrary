4d15ea2f0e7912bd4e119e1614c4d05bf63453c6
Jason <elohwhydee@gmail.com>
2014-05-30 14:35:43 -0400

    Added model

--- library_app/book_entry/admin.py
+++ library_app/book_entry/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from book_entry.models import Book
+
+admin.site.register(Book)
 
 # Register your models here.
--- library_app/book_entry/models.py
+++ library_app/book_entry/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    isbn = models.IntegerField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    ddc = models.CharField(max_length=20)
+    dewey_desc = models.CharField(max_length=200)
+    description = models.CharField(max_length=4000)
+    location = models.CharField(max_length=200)
+    owner = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.title
