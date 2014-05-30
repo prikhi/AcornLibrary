b2cac6b15feb3d9613c4e200a683f4857bbd72d2
Jason <elohwhydee@gmail.com>
2014-05-30 15:59:42 -0400

    Add a simple view

--- library_app/book_entry/views.py
+++ library_app/book_entry/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from book_entry.models import Book
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world!")
+
+def detail(request, isbn):
+    book = Book.objects.get(isbn=isbn)
+    title = book.title
+    return HttpResponse("Title: %s" % title)
--- library_app/library_app/urls.py
+++ library_app/library_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'library_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^book_entry/', include('book_entry.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
