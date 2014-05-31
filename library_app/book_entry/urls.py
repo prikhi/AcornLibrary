3bb867e10ae61ed99696e5b854afab07ac5f8c15
Jason <elohwhydee@gmail.com>
2014-05-31 14:43:11 -0400

    Add ISBN input

---
+++ library_app/book_entry/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from book_entry import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.lookup, name='lookup'),
+    url(r'^do_lookup$', views.do_lookup, name='do_lookup'),
+    url(r'^(?P<isbn>\d+)/$', views.detail, name='detail')
+)
--- library_app/book_entry/views.py
+++ library_app/book_entry/views.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse, Http404
 from book_entry.models import Book
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world!")
+def lookup(request):
+    context = {'book': 1}
+    return render(request, 'book_entry/lookup.html', context)
+
+def do_lookup(request):
+    isbn = request.POST['isbn']
+    
+    return HttpResponse(isbn)
 
 def detail(request, isbn):
     book = get_object_or_404(Book, isbn=isbn)
