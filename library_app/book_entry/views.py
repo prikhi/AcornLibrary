dbe037557159985f5200028e5c1b5de2970c48b7
Jason <elohwhydee@gmail.com>
2014-05-31 15:35:59 -0400

    Add simple web scraping

--- library_app/book_entry/views.py
+++ library_app/book_entry/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, Http404
 
 from book_entry.models import Book
 
+from lxml import html
+import requests
+
 # Create your views here.
 def lookup(request):
     context = {'book': 1}
@@ -10,8 +13,13 @@ def lookup(request):
 
 def do_lookup(request):
     isbn = request.POST['isbn']
-    
-    return HttpResponse(isbn)
+
+    page = requests.get(''.join(['http://classify.oclc.org/classify2/ClassifyDemo?search-standnum-txt=', isbn]))
+    tree = html.fromstring(page.text)
+    title = tree.xpath('//*[@id="display-Summary"]/dl/dd[1]/text()')
+    author = tree.xpath('//*[@id="display-Summary"]/dl/dd[2]/a[1]/text()')
+    ddc = tree.xpath('//*[@id="classSummaryData"]/tbody/tr[1]/td[2]/text()')
+    return HttpResponse(ddc)
 
 def detail(request, isbn):
     book = get_object_or_404(Book, isbn=isbn)
