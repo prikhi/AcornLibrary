0a65298928e7f8965cc93dfc3e72bccf2ab6a41b
Jason <elohwhydee@gmail.com>
2014-06-14 12:00:17 -0400

    Use multiple settings files

--- library_app/books/models.py
+++ library_app/books/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.forms import ModelForm
 
 # Create your models here.
+
+
 class Book(models.Model):
     isbn = models.IntegerField()
     title = models.CharField(max_length=200)
@@ -14,6 +16,7 @@ class Book(models.Model):
 
     def __unicode__(self):
         return self.title
+
 
 class BookForm(ModelForm):
     class Meta:
--- library_app/books/views.py
+++ library_app/books/views.py
@@ -9,6 +9,7 @@ import requests
 
 # Create your views here.
 
+
 def entry(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
@@ -19,15 +20,17 @@ def entry(request):
         'form': form,
     })
 
+
 def lookup(request):
     return render(request, 'books/lookup.html', {})
+
 
 def old_entry(request):
     isbn = request.POST['isbn']
 
     page = requests.get(''.join(['http://classify.oclc.org/classify2/ClassifyDemo?search-standnum-txt=', isbn]))
     tree = html.fromstring(page.text)
-    if not tree.xpath('//*[@id="display-Summary"]/dl/dd[1]/text()') : # Oh dear god, please fix me
+    if not tree.xpath('//*[@id="display-Summary"]/dl/dd[1]/text()'):  # Oh dear god, please fix me
         page = requests.get(''.join(['http://classify.oclc.org', ''.join(tree.xpath('//*[@id="results-table"]/tbody/tr[1]/td[1]/span[1]/a/@href'))]))
         tree = html.fromstring(page.text)
     title = tree.xpath('//*[@id="display-Summary"]/dl/dd[1]/text()')
@@ -35,22 +38,23 @@ def old_entry(request):
     ddc = tree.xpath('//*[@id="classSummaryData"]/tbody/tr[1]/td[2]/text()')
 
     context = {'isbn': isbn,
-               'title': ''.join(title), #Why are these lists?
+               'title': ''.join(title),  # Why are these lists?
                'author': ''.join(author),
-               'ddc': ''.join(ddc)
-    }
+               'ddc': ''.join(ddc)}
 
     return render(request, 'books/entry.html', context)
 
+
 def save(request):
-    b = Book(isbn = request.POST.get('isbn', 0),
-             title = request.POST.get('title', ''),
-             author = request.POST.get('author', ''),
-             ddc = request.POST.get('ddc', ''))
+    b = Book(isbn=request.POST.get('isbn', 0),
+             title=request.POST.get('title', ''),
+             author=request.POST.get('author', ''),
+             ddc=request.POST.get('ddc', ''))
     b.save()
     return redirect('/')
 
+
 def detail(request, isbn):
     book = get_object_or_404(Book, isbn=isbn)
-    context = {'book': book,}
+    context = {'book': book, }
     return render(request, 'books/index.html', context)
