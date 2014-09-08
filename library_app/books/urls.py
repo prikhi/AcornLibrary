9ed5e4f676fbf11ccdc26b9919df302cf310bdc9
Jason <elohwhydee@gmail.com>
2014-09-08 17:42:06 -0400

    Use class based views to fix update bug

--- library_app/books/urls.py
+++ library_app/books/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import patterns, url, include
 from haystack.views import SearchView, search_view_factory
 from haystack.forms import SearchForm
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
 
 from books import views
+from books.models import Book, BookForm
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="books/index.html")),
-    #url(r'^do_lookup$', views.do_lookup, name='do_lookup'),
-    url(r'^entry$', views.entry, name='entry'),
-    url(r'^entry/(\d+)$', views.entry, name='edit'),
+    url(r'^entry$', views.CreateBookView.as_view(), name='entry'),
+    url(r'^entry/(?P<pk>\d+)$', views.UpdateBookView.as_view(), name='update'),
     url(r'^lookup/$', views.lookup, name='lookup'),
     url(r'^subjects$', views.subjects, name='subjects'),
     url(r'^(?P<isbn>\d+)/$', views.detail, name='detail'),
--- library_app/books/views.py
+++ library_app/books/views.py
@@ -2,12 +2,49 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.utils import simplejson
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic.edit import UpdateView, CreateView
+from django.core.urlresolvers import reverse
 
 from books.models import Book
 from books.models import BookForm
 from books import utils
 
 # Create your views here.
+class CreateBookView(SuccessMessageMixin, CreateView):
+
+    model = Book
+    form = BookForm
+    template_name = 'books/entry.html'
+    success_message = "%(title)s was added successfully"
+    
+    def get_success_url(self):
+        return reverse('books:entry')
+        
+    def get_context_data(self, **kwargs):
+    
+        context = super(CreateBookView, self).get_context_data(**kwargs)
+        context['action'] = reverse('books:entry')
+        context['create'] = True
+        
+        return context
+        
+
+class UpdateBookView(SuccessMessageMixin, UpdateView):
+    
+    model = Book
+    template_name = 'books/entry.html'
+    success_message = "%(title)s was updated successfully"
+    
+    def get_success_url(self):
+        return reverse('books:entry')
+    
+    def get_context_data(self, **kwargs):
+    
+        context = super(UpdateBookView, self).get_context_data(**kwargs)
+        context['action'] = reverse('books:update', kwargs={'pk': self.get_object().id})
+        
+        return context
 
 
 def entry(request, book=None):
