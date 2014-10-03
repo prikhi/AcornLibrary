2bcf035b96f401c802183393c161a88196713b56
Jason <elohwhydee@gmail.com>
2014-10-03 13:52:19 -0400

    Animate results rows expanding

--- library_app/books/static/books/js/jExpand.js
+++ library_app/books/static/books/js/jExpand.js
@@ -3,12 +3,135 @@
         var element = this;
 
         $(element).find("tr:odd").addClass("odd");
+        $(element).find("tr:not(.odd)").addClass("even");
+        $(element).find("tr:first-child").removeClass("even");
         $(element).find("tr:not(.odd)").hide();
+        $(element).find("tr:not(.odd)").addClass("collapsed");
         $(element).find("tr:first-child").show();
 
         $(element).find("tr.odd").click(function() {
-            $(this).next("tr").toggle();
+            if ($(this).next("tr").hasClass("collapsed")) {
+                $(this).next("tr").slideRow('down');
+                $(this).next("tr").removeClass("collapsed");
+            } else {
+                $(this).next("tr").slideRow('up');
+                $(this).next("tr").addClass("collapsed");
+            }
+        });
+        
+        $(element).find("tr.even").click(function() {
+            $(this).slideRow('up');
+            $(this).addClass("collapsed");
         });
         
     }    
 })(jQuery); 
+
+(function($) {
+var sR = {
+    defaults: {
+        slideSpeed: 400,
+        easing: false,
+        callback: false     
+    },
+    thisCallArgs: {
+        slideSpeed: 400,
+        easing: false,
+        callback: false
+    },
+    methods: {
+        up: function (arg1,arg2,arg3) {
+            if(typeof arg1 == 'object') {
+                for(p in arg1) {
+                    sR.thisCallArgs.eval(p) = arg1[p];
+                }
+            }else if(typeof arg1 != 'undefined' && (typeof arg1 == 'number' || arg1 == 'slow' || arg1 == 'fast')) {
+                sR.thisCallArgs.slideSpeed = arg1;
+            }else{
+                sR.thisCallArgs.slideSpeed = sR.defaults.slideSpeed;
+            }
+
+            if(typeof arg2 == 'string'){
+                sR.thisCallArgs.easing = arg2;
+            }else if(typeof arg2 == 'function'){
+                sR.thisCallArgs.callback = arg2;
+            }else if(typeof arg2 == 'undefined') {
+                sR.thisCallArgs.easing = sR.defaults.easing;    
+            }
+            if(typeof arg3 == 'function') {
+                sR.thisCallArgs.callback = arg3;
+            }else if(typeof arg3 == 'undefined' && typeof arg2 != 'function'){
+                sR.thisCallArgs.callback = sR.defaults.callback;    
+            }
+            var $cells = $(this).find('td');
+            $cells.wrapInner('<div class="slideRowUp" />');
+            var currentPadding = $cells.css('padding');
+            $cellContentWrappers = $(this).find('.slideRowUp');
+            $cellContentWrappers.slideUp(sR.thisCallArgs.slideSpeed,sR.thisCallArgs.easing).parent().animate({
+                                                                                                                paddingTop: '0px',
+                                                                                                                paddingBottom: '0px'},{
+                                                                                                                complete: function () {
+                                                                                                                    $(this).children('.slideRowUp').replaceWith($(this).children('.slideRowUp').contents());
+                                                                                                                    $(this).parent().css({'display':'none'});
+                                                                                                                    $(this).css({'padding': currentPadding});
+                                                                                                                }});
+            var wait = setInterval(function () {
+                if($cellContentWrappers.is(':animated') === false) {
+                    clearInterval(wait);
+                    if(typeof sR.thisCallArgs.callback == 'function') {
+                        sR.thisCallArgs.callback.call(this);
+                    }
+                }
+            }, 100);                                                                                                    
+            return $(this);
+        },
+        down: function (arg1,arg2,arg3) {
+            if(typeof arg1 == 'object') {
+                for(p in arg1) {
+                    sR.thisCallArgs.eval(p) = arg1[p];
+                }
+            }else if(typeof arg1 != 'undefined' && (typeof arg1 == 'number' || arg1 == 'slow' || arg1 == 'fast')) {
+                sR.thisCallArgs.slideSpeed = arg1;
+            }else{
+                sR.thisCallArgs.slideSpeed = sR.defaults.slideSpeed;
+            }
+
+            if(typeof arg2 == 'string'){
+                sR.thisCallArgs.easing = arg2;
+            }else if(typeof arg2 == 'function'){
+                sR.thisCallArgs.callback = arg2;
+            }else if(typeof arg2 == 'undefined') {
+                sR.thisCallArgs.easing = sR.defaults.easing;    
+            }
+            if(typeof arg3 == 'function') {
+                sR.thisCallArgs.callback = arg3;
+            }else if(typeof arg3 == 'undefined' && typeof arg2 != 'function'){
+                sR.thisCallArgs.callback = sR.defaults.callback;    
+            }
+            var $cells = $(this).find('td');
+            $cells.wrapInner('<div class="slideRowDown" style="display:none;" />');
+            $cellContentWrappers = $cells.find('.slideRowDown');
+            $(this).show();
+            $cellContentWrappers.slideDown(sR.thisCallArgs.slideSpeed, sR.thisCallArgs.easing, function() { $(this).replaceWith( $(this).contents()); });
+
+            var wait = setInterval(function () {
+                if($cellContentWrappers.is(':animated') === false) {
+                    clearInterval(wait);
+                    if(typeof sR.thisCallArgs.callback == 'function') {
+                        sR.thisCallArgs.callback.call(this);
+                    }
+                }
+            }, 100);
+            return $(this);
+        }
+    }
+};
+
+$.fn.slideRow = function(method,arg1,arg2,arg3) {
+    if(typeof method != 'undefined') {
+        if(sR.methods[method]) {
+            return sR.methods[method].apply(this, Array.prototype.slice.call(arguments,1));
+        }
+    }
+};
+})(jQuery);
