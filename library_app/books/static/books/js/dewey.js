0926c32a5b7dbfab9954952355714b3712654d87
Jason <elohwhydee@gmail.com>
2014-10-09 13:11:48 -0400

    Enable state plguin for jsTree

--- library_app/books/static/books/js/dewey.js
+++ library_app/books/static/books/js/dewey.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-$('#dewey_tree').jstree().on("select_node.jstree", function (e, data) {
+$('#dewey_tree').jstree({"plugins" : [ "state" ]}).on("select_node.jstree", function (e, data) {
            document.location = data.instance.get_node(data.node, true).children('a').attr('href');
     });;
 
