9f7bcd7c5457263a7428518fea6ef70631f8e5d9
Jason <elohwhydee@gmail.com>
2014-10-09 11:57:15 -0400

    Try to find best match on multiple listings

--- library_app/books/utils.py
+++ library_app/books/utils.py
@@ -3,6 +3,7 @@ from lxml import html, etree, objectify
 import urllib
 import json
 import re
+import string
 
 def tags_from_strings(tag_string):
     return [t.strip() for t in tag_string.split('%') if t.strip()]
@@ -12,7 +13,8 @@ def string_from_tags(tags):
     return '%'.join(t.name for t in tags)
     
 def get_book_info(request):
-    success=True     
+    success=True
+    # Google's title and author data is cleaner, and they sometimes have a description, so we try them first     
     isbn = request.GET['isbn']
     api_key = 'AIzaSyD8fIqUMR_jtZ9PxS9j9uqnuoD3RKx6fB8'
     url = ('https://www.googleapis.com/books/v1/volumes?q=isbn:%s&key=%s') % (isbn, api_key)
@@ -35,21 +37,48 @@ def get_book_info(request):
         description=''
         success = False
     
+    # Next we try OCLC classify to get the dewey decimal and subjects, and also other information if google couldn't find it
     url = ('http://classify.oclc.org/classify2/Classify?isbn=%s') % (isbn)
     root = objectify.fromstring(urllib.request.urlopen(url).read())
     response_code = root.response.attrib['code']
     if response_code == '4':
-        owi = root.works.work[0].attrib['owi']
+        # Multiple listings were found. Oh joy. We need to figure out which one of the listings corresponds to the book we're looking for. We do this by getting the titles of hte listings, and comparing them word by word to the google title. We assign a score to each one based on how many matching words there are, and pick the highest scoring one.
+        oclc_titles = [ el.attrib['title'] for el in root.works.iterchildren() ]
+        max_score = 0
+        highest_scoring_index = 0
+        if title:
+            exclude = string.punctuation
+            google_title = title.lower()
+            google_title = ''.join(ch for ch in google_title if ch not in exclude)
+            google_title_split = google_title.split()
+            for index, oclc_title in enumerate(oclc_titles):
+                score=0
+                ot = oclc_title.lower()
+                ot = ''.join(ch for ch in ot if ch not in exclude)
+                oclc_title_split = ot.split()
+                for x in google_title_split:
+                    for y in oclc_title_split:
+                        if x==y:
+                            # square the length to more heavily weight longer matches
+                            score = score+(len(x)**2)
+                if score > max_score:
+                    max_score=score
+                    highest_scoring_index=index        
+        
+        #Hopefully we've found the best match, use the owi identifier to load the book information
+        owi = root.works.work[highest_scoring_index].attrib['owi']
         url = ('http://classify.oclc.org/classify2/Classify?owi=%s') % (owi)
         root = objectify.fromstring(urllib.request.urlopen(url).read())
         response_code = root.response.attrib['code']
     if response_code == '0' or response_code == '2':
+        # We successfully got the book information
         dewey_decimal = root.recommendations.ddc.mostPopular.attrib['nsfa']
         if dewey_decimal=='FIC':
             dewey_decimal = root.recommendations.ddc.mostPopular[1].attrib['nsfa']
         subjects = [ el.text for el in root.recommendations.fast.headings.iterchildren()]
         if not authors:
             authors = []
+            # do a whole bunch of bullshit to clean up the author data. Take out birth/death year, reverse "last, first" format
             for el in root.authors.iterchildren():
                 text = el.text
                 digit = re.search('\d', text)
